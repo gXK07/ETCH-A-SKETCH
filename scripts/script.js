7ab5dc374aa59cd2904bca68349ecdddb3397393
Greg <gregoire.ansard@gmail.com>
2021-05-21 11:50:38 +0200

    added the reset button

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
     let cases = new Array(256);
+    let reset = document.querySelector("#reset");
 
     for(i = 0; i<256; i++){
     cases[i] = document.createElement('div');
@@ -6,6 +7,20 @@
     }
     function addHovered(e){
         e.target.classList.add("hovered");
-        console.log(e.target);
+    }
+    /*
+    // redifined the number of quare in the grid (lenght of the array "cases")
+    function reSize(string){
+        //instruction
+    }
+    */
+    function doReset(){
+        for(i=0; i<cases.length; i++){
+            cases[i].classList.remove("hovered");
+            /*let squareWanted = prompt("How many scare do you want ?");
+            reSize(squareWanted);
+            */
+        }
     }
     cases.forEach(x => x.addEventListener('mouseover', addHovered));
+    reset.addEventListener('click', doReset);
